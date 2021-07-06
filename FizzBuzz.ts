19987b595b4833d9035070feb8875fcb9a591e66
Jack Parkinson <Jack.Parkinson@softwire.com>
2021-07-06 12:31:51 +0100

    Make it nicer -- adding types, etc

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,10 +1,10 @@
 function fizzBuzzLoop(noIters: number) {
-    for (var i = 0; i <= noIters; i++){ 
+    for (var i: number = 0; i <= noIters; i++){ 
         fizzBuzz(i);
     }
 }
 function fizzBuzz(intToReturn: number) {
-    var fizzBuzzString = (intToReturn % 3 === 0 ? "Fizz" : "") + (intToReturn % 5 === 0 ? "Buzz" : "");
+    var fizzBuzzString: string = (intToReturn % 3 === 0 ? "Fizz" : "") + (intToReturn % 5 === 0 ? "Buzz" : "");
     console.log(fizzBuzzString || String(intToReturn));
 }
 
