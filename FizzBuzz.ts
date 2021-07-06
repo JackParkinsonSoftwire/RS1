223e7b2afa1e7fdce61f7dd31c8e0df397c708cf
Jack Parkinson <Jack.Parkinson@softwire.com>
2021-07-06 14:16:56 +0100

    FizzBuzz done

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,11 +1,42 @@
-function fizzBuzzLoop(noIters: number) {
-    for (var i: number = 0; i <= noIters; i++){ 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function fizzBuzzLoop(noIters: number):void {
+    for (var i: number = 1; i <= noIters; i++){ 
         fizzBuzz(i);
     }
 }
-function fizzBuzz(intToReturn: number) {
-    var fizzBuzzString: string = (intToReturn % 3 === 0 ? "Fizz" : "") + (intToReturn % 5 === 0 ? "Buzz" : "");
+function fizzBuzz(intToReturn: number):void {
+    var fizzBuzzArr: string[] = [];
+    if (intToReturn % 3 === 0){
+        fizzBuzzArr.push("Fizz");
+    } 
+    if (intToReturn % 13 === 0){
+        fizzBuzzArr.push("Fezz");
+    }
+    if (intToReturn % 5 === 0){
+        fizzBuzzArr.push("Buzz");
+    } 
+    if (intToReturn % 7 === 0){
+        fizzBuzzArr.push("Bang");
+    } 
+    if (intToReturn % 11 === 0){
+        fizzBuzzArr.filter(value => value == "Fezz").push("Bong");
+    }
+    if (intToReturn % 17){
+        fizzBuzzArr = fizzBuzzArr.reverse()
+    }
+    var fizzBuzzString: string = fizzBuzzArr.join("");
     console.log(fizzBuzzString || String(intToReturn));
 }
 
-fizzBuzzLoop(15);
+const fizzBuzzAsk = () => rl.question('How many different FizzBuzz do you want me to compute? >', (response:number) => {
+  fizzBuzzLoop(response);
+  rl.close();
+});
+
+fizzBuzzAsk();
